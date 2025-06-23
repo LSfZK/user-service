3fd1337db98c06d15dfb0bb94e86fc29841f60b1
jerry8534 <jerryjjinu@gmail.com>
2025-06-24 00:19:39 +0900

    first commit

---
+++ src/test/java/lsfzk/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lsfzk.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
